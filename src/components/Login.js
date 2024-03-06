0ab10990755252a3de83d29fe366f561d38046ca
calle fransson <calle-fransson@hotmail.com>
2024-03-06 20:43:40 +0100

    created route for different roles when login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,51 +1,83 @@
-import React, { useState } from "react";
+import React, { useState,useEffect  } from "react";
 import axios from "axios";
-import { BrowserRouter as Router,Route,Routes, Navigate } from "react-router-dom";
-import Employee from "../pages/Employee";
+import { useNavigate  } from "react-router-dom";
+
 
 const Login = () => {
-  const [employeID, setEmployeID] = useState('');
-  const [password, setPassword] = useState('');
-  const [loggedIn, setLoggedIn] = useState(false);
-  async function login(){
-    try{
-      const response = await axios.post("http://localhost:4000/submitFormToNotion",{
-        employeID,
-        password
-      });
+  const [employeID, setEmployeID] = useState("");
+  const [password, setPassword] = useState("");
+  const [userRole, setUserRole] = useState("");
+  const history = useNavigate();
 
-      if(response.status === 200){
-        localStorage.setItem("user-info",JSON.stringify(response.data))
-        setLoggedIn(true);
-        // return(
-        //   // <Router>
-        //   //   <Routes>
-        //   //     <Route exact path="/pages" component={Employee} />
-        //   //   </Routes>
-        //   // </Router>
-        // );
-      } else {
-        console.log('wrong employeid or password')
+  useEffect(() => {
+    if (userRole) {
+      switch (userRole) {
+        case "Anställd":
+          history("/Employee");
+          break;
+        case "Chef":
+          history("/Manager");
+          break;
+        case "Projektledare":
+          history("/ProjectManager");
+          break;
+        default:
+          history("/default-page");
+          break;
       }
-    } catch(error) {
-      console.log('Error: ',error);
+    }
+  }, [userRole, history]);
+
+  async function LoginHandler() {
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/submitFormToNotion",
+        {
+          employeID,
+          password
+        }
+      );
+
+      if (response.status === 200) {
+        const userData = response.data;
+        setUserRole(userData.userRoles[0]);
+
+        }
+    } catch (error) {
+      console.log("Error: ", error);
     }
   }
-
   return (
     <div>
-      <h1>Logga in</h1>
-      <form onSubmit={(e) => {e.preventDefault(); login();}}>
+        <h1>Logga in</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          LoginHandler();
+        }}
+      >
         <label htmlFor="employeID">* Anställningsid</label>
-        <input type="text" name="employeID" value={employeID} placeholder="Anställningsid" required onChange={(e) => setEmployeID(e.target.value)} />
+        <input
+          type="text"
+          name="employeID"
+          value={employeID}
+          placeholder="Anställningsid"
+          required
+          onChange={(e) => setEmployeID(e.target.value)}
+        />
         <label htmlFor="password">* Lösenord</label>
-        <input type="password" name="password" value={password} placeholder="Lösenord" required onChange={(e) => setPassword(e.target.value)} />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          placeholder="Lösenord"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Logga in</button>
       </form>
-      {loggedIn && <Router><Navigate to={<Employee/>} /></Router>}
     </div>
   );
 };
-
 
 export default Login;
