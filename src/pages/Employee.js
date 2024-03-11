ee0620589cb308aea6ce3622227bc9939a630061
Glind99 <gustav.lind.net23@edu.edugrade.se>
2024-03-11 10:55:07 +0100

    navbar

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import FetchUsername from '../components/FetchUsername'
+import MyNavbar from '../components/navbar'
 
 const Employee = () => {
   return (
     <div>
+      <MyNavbar></MyNavbar>
       <FetchUsername/>
     </div>
   )
