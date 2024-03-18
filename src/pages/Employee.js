ec63eabcbf11d517369f0e451dafe64f762bfaff
Josefin Mikaelsson <josefin.mikaelsson.NET23@edu.edugrade.se>
2024-03-18 08:30:21 +0100

    ModalPopUp för att rapportera in tid

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import FetchUsername from '../components/FetchUsername'
 import MyNavbar from '../components/navbar'
+import Modalpopup from '../components/ModalPopUp'
 
 const Employee = () => {
   return (
     <div>
       <MyNavbar></MyNavbar>
       <FetchUsername/>
+      <Modalpopup/>
     </div>
   )
 }
